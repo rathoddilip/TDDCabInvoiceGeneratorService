728a6c069a2e787b1557c14d61ed7b84f12f0a35
diliprathod32 <diliprathod32@gmail.com>
2021-03-14 16:08:23 +0530

    [Dilip] CalculateFare

---
+++ CabInvoiceGeneratorServiceNUnitTesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using TDDCabInvoiceGeneratorService;
+namespace CabInvoiceGeneratorServiceNUnitTesting
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator;
+        [SetUp]
+        public void Setup()
+        {
+            invoiceGenerator = new InvoiceGenerator();
+        }
+    }
+}
