164731a386747850a480c3d2568637ddaa69fe20
diliprathod32 <diliprathod32@gmail.com>
2021-03-14 15:45:14 +0530

    [Dilip] WelcomeToCabInvoiceGeneratorService

---
+++ TDDCabInvoiceGeneratorService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDCabInvoiceGeneratorService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cab invoice generator service");
+        }
+    }
+}
